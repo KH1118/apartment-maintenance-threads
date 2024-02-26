329a660743bdc7a78352709a9e386459dcb0a429
KH1118 <s1230119@gmail.com>
2024-02-26 22:35:56 +0900

    HTML作成

--- app/src/index.php
+++ app/src/index.php
@@ -1,2 +1,36 @@
-<?php
-echo "Success";
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理物件掲示板</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="title">管理物件掲示板</h1>
+        <hr>
+    </header>
+
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>【物件名】</span>
+                <h1>○○レジデンス（仮名称）</h1>
+            </div>
+            <section>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>異常箇所:</span>
+                            <p class="username">1階共用部</p>
+                            <time>:2024/02/26 12:00</time>
+                        </div>
+                        <p class="comment">ブラッシングが必要です。</p>
+                    </div>
+                </article>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
