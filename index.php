78fde9fee2996d8d71aad2e25579065e20e7014a
KH1118 <s1230119@gmail.com>
2024-03-03 11:30:06 +0900

    CSSでスタイリング

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理物件掲示板</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
@@ -21,9 +23,9 @@
             <section>
                 <article>
                     <div class="wrapper">
-                        <div class="nameArea">
+                        <div class="locationArea">
                             <span>異常箇所:</span>
-                            <p class="username">1階共用部</p>
+                            <p class="location">1階共用部</p>
                             <time>:2024/02/26 12:00</time>
                         </div>
                         <p class="comment">ブラッシングが必要です。</p>
